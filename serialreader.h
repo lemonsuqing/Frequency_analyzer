6a91a761db3feb4626b3463ff97483289ca1a180
lemonsuqing <1428828189@qq.com>
2025-07-18 17:09:45 +0800

    project init, usart success

---
+++ serialreader.h
@@ -0,0 +1,21 @@
+// #ifndef SERIALREADER_H
+// #define SERIALREADER_H
+
+// #include <QObject>
+// #include <QSerialPort>
+
+// class SerialReader : public QObject {
+//     Q_OBJECT
+
+// public:
+//     SerialReader(const QString& portName, int baudRate = 115200);
+//     void start();
+
+// private slots:
+//     void handleReadyRead();
+
+// private:
+//     QSerialPort serial;
+// };
+
+// #endif // SERIALREADER_H
